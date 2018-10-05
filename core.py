cf090b98afdc04d57540afe106be679a4fcca833
vincentle <vince.le95@gmail.com>
2018-10-05 10:22:51 -0400

    Added first Python program

---
+++ core.py
@@ -0,0 +1,11 @@
+import turtle
+while True:
+    print('hello, type exit to exit')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    else:
+        print('you typed ' + response + ' you dingus')
+        print('you typed ' + response + ' you dingus')
+        print('you typed ' + response + ' you dingus')
+        print('you typed ' + response + ' you dingus')
